841c7ee08ceb747580d58e7848c811dc6edcfc28
hlrmp <haysjhune@gmail.com>
2023-11-24 17:26:46 +0800

    Add project files.

---
+++ BasicCalculator/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace BasicCalculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblanswer = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.lblanswer);
+            this.panel1.Controls.Add(this.lbltotal);
+            this.panel1.Location = new System.Drawing.Point(47, 226);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 100);
+            this.panel1.TabIndex = 12;
+            // 
+            // lblanswer
+            // 
+            this.lblanswer.AutoSize = true;
+            this.lblanswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblanswer.ForeColor = System.Drawing.Color.Lime;
+            this.lblanswer.Location = new System.Drawing.Point(11, 44);
+            this.lblanswer.Name = "lblanswer";
+            this.lblanswer.Size = new System.Drawing.Size(111, 33);
+            this.lblanswer.TabIndex = 1;
+            this.lblanswer.Text = "000000";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.ForeColor = System.Drawing.Color.Lime;
+            this.lbltotal.Location = new System.Drawing.Point(14, 12);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(49, 18);
+            this.lbltotal.TabIndex = 0;
+            this.lbltotal.Text = "Total :";
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.Location = new System.Drawing.Point(163, 387);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(112, 33);
+            this.btnCompute.TabIndex = 11;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Location = new System.Drawing.Point(234, 135);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(41, 21);
+            this.cbOperator.TabIndex = 10;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(47, 171);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(245, 20);
+            this.txt2.TabIndex = 9;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(47, 93);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(245, 20);
+            this.txt1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.Text = "FrmBasicCalculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblanswer;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BasicCalculator/FrmBasicCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatePrivateAssembly;
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+            operators();
+        }
+
+        public void operators()
+        {
+            ArrayList array = new ArrayList();
+            array.Add("-");
+            array.Add("+");
+            array.Add("*");
+            array.Add("/");
+
+            foreach (string op in array)
+            {
+                cbOperator.Items.Add(op);
+            }
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            float num1 = Convert.ToInt32(txt1.Text);
+            float num2 = Convert.ToInt32(txt2.Text);
+            float total;
+
+            if(cbOperator.Text.ToString().Equals("+"))
+            {
+                total = BasicComputation.Addition(num1, num2);
+                lblanswer.Text = total.ToString();
+            }
+           else if (cbOperator.Text.ToString().Equals("-"))
+            {
+                total = BasicComputation.Substraction(num1, num2);
+                lblanswer.Text = total.ToString();
+            }
+           else if (cbOperator.Text.ToString().Equals("*"))
+            {
+                total = BasicComputation.Multiplication(num1, num2);
+                lblanswer.Text = total.ToString();
+            }
+            else if (cbOperator.Text.ToString().Equals("/"))
+            {
+                total = BasicComputation.Division(num1, num2);
+                lblanswer.Text = total.ToString();
+            }
+
+
+        }
+
+       
+    }
+}
